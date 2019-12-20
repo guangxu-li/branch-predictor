6e81974d0556bfe24a29e6ce6a2cf95c676e5f43
Guangxu Li <guangxu.li@hotmail.com>
2019-12-20 05:35:56 +0000

    release

--- branchsimulator.cpp
+++ branchsimulator.cpp
@@ -27,36 +27,37 @@ int main(int argc, char **argv)
 	ifstream trace;
 	trace.open(argv[2]);
 
-	bitset<32> pc;
+	unsigned long pc;
 	bool taken;
-	trace >> std::dec >> pc >> taken;
+	trace >> std::hex >> pc >> taken;
+	bitset<32> ppc = pc;
 
 	// define bhr
-	bitset<8> bhr = pow(2, k) - 1;
+	int bhr = pow(2, k) - 1;
 
 	// define pht
-	vector<vector<int>> pht(pow(2, k) - 1, vector<int>(pow(2, m) - 1));
+	vector<vector<int>> pht(pow(2, k), vector<int>(pow(2, m)));
 
 	// history k
-	int row;
-	int col;
-	for (col = 0; col < pow(2, k) - 1; col++)
+	for (int col = 0; col < pow(2, k); col++)
 	{
 		// index m
-		for (row = 0; row < pow(2, m) - 1; row++)
+		for (int row = 0; row < pow(2, m); row++)
 		{
-			cout << col << " " << row << endl;
 			pht[col][row] = 3;
 		}
 	}
 
 	while (!trace.eof())
 	{
-		string pc_substr = pc.to_string().substr(32 - m, m);
-		unsigned long mm = bitset<32>(pc_substr).to_ulong();
+		// substract m lsb from pc
+		string pc_substr = ppc.to_string().substr(32 - m, m);
+
+		// initial / update mm <- row of pht
+		unsigned long mm = bitset<32>(pc_substr).to_ulong() - 1;
 
 		// predict
-		bool prediction = pht[k][mm] >= 2 ? true : false;
+		bool prediction = pht[bhr][mm] >= 2 ? true : false;
 		out << prediction << endl;
 
 		// update pht
@@ -65,20 +66,20 @@ int main(int argc, char **argv)
 			// taken
 			// pht == 00 -> 01
 			// pht == 01 / 10 / 11 -> 11
-			pht[k][mm] = pht[k][mm] == 0 ? 1 : 3;
+			pht[bhr][mm] = pht[bhr][mm] == 0 ? 1 : 3;
 		}
 		else
 		{
 			// not taken
 			// pht == 11 -> 10
 			// pht == 10 / 01 / 00 -> 0
-			pht[k][mm] = pht[k][mm] == 3 ? 2 : 0;
+			pht[bhr][mm] = pht[bhr][mm] == 3 ? 2 : 0;
 		}
 		// update bhr
-		bhr = bhr.to_ulong() - pow(2, k - 1) + taken;
-
+		bhr = bitset<32>(bitset<32>(bhr).to_string().substr(33 - k, k)).to_ulong() * 2 + taken; // 32 - k + 1
 		// next branch
-		trace >> std::dec >> pc >> taken;
+		trace >> std::hex >> pc >> taken;
+		ppc = pc;
 	}
 
 	trace.close();
